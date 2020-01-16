894751b1da313a7e33a19ebf1d862ab78dbded6c
Shiqing <shiqing-thu18@yandex.com>
2020-01-17 01:16:13 +0800

    Switch to premultiplied alpha and fix blending

--- sys/v3d/shader_tex_chroma_alpha.fx
+++ sys/v3d/shader_tex_chroma_alpha.fx
@@ -1,11 +1,11 @@
 # Uniform 0/1: texture
 # Varying 0/1: texture coordinate (0 to 1)
 # Varying 2/3/4/5: chroma (RGBA)
+# Premultiplied alpha for everything
 
 # Load texture coordinates
 nop; fmul r0, vary, ra15
-fadd r0, r0, r5; nop
-nop; fmul r1, vary, ra15
+fadd r0, r0, r5; fmul r1, vary, ra15
 fadd r1, r1, r5; nop
 
 # Issue TMU read
@@ -27,28 +27,25 @@ nop; v8muld ra0, r3, r4; loadc
 # ra0 = tinted texture, R/A (R represents any chroma channel hereafter)
 # r4 = canvas, Rcan/Acan
 
+# R' = R + Rcan * (1-A)
+# A' = A + Acan - A*Acan
+
 fadd r0, 0.0, r4.8d; nop
 fadd r1, 0.0, ra0.8d; nop
 fsub r2, 1.0, r1; mov rb1.8abcd, r1
 nop; mov ra2.8abcd, r2
-nop; fmul r2, r0, r1
+nop; fmul rb3, r0, r1
 # r0 = Acan
 # r1 = A
 # rb1 = A (packed)
 # ra2 = 1-A (packed)
-# r2 = A*Acan
+# rb3 = A*Acan
 
-# A' = 1 - (1-A) * (1-Acan) = A + Acan - A*Acan
-# R' = Rcan * (1-A) + R * A
-fadd r0, r0, r1; v8muld ra4, r4, ra2
-fadd r0, r0, r2; v8muld r1, ra0, rb1
-v8adds r3, ra4, r1; nop
+fadd r0, r0, r1; v8muld r2, r4, ra2
+fsub r0, r0, rb3; v8adds r3, r2, ra0
 # r0 (overwrite) = A'
-# ra4 = Rcan * (1-A) (packed RGB)
-# r1 (overwrite) = R * A (packed RGB)
+# r2 = Rcan * (1-A) (packed RGB)
 # r3 = R' (packed RGB)
-
-nop; mov r3.8d, r0
 
 mov tlbc, r3; thrend
 nop; nop
--- sys/v3d_test.c
+++ sys/v3d_test.c
@@ -20,34 +20,34 @@ void doda()
   v.x = 250.0f; v.y = 100.0f;
   v.varyings[0] = 0.5f;
   v.varyings[1] = 0.0f;
-  v.varyings[2] = 1.0f;
-  v.varyings[3] = 1.0f;
-  v.varyings[4] = 1.0f;
-  v.varyings[5] = 1.0f;
+  v.varyings[2] = 0.3f;
+  v.varyings[3] = 0.3f;
+  v.varyings[4] = 0.3f;
+  v.varyings[5] = 0.3f;
   v3d_vertarr_put(&va1, 0, &v, 1);
   v.x = 100.0f; v.y = 400.0f;
   v.varyings[0] = 0.0f;
   v.varyings[1] = 1.0f;
-  v.varyings[2] = 0.5f;
-  v.varyings[3] = 1.0f;
-  v.varyings[4] = 1.0f;
-  v.varyings[5] = 1.0f;
+  v.varyings[2] = 0.3f;
+  v.varyings[3] = 0.3f;
+  v.varyings[4] = 0.3f;
+  v.varyings[5] = 0.3f;
   v3d_vertarr_put(&va1, 1, &v, 1);
   v.x = 400.0f; v.y = 400.0f;
   v.varyings[0] = 1.0f;
   v.varyings[1] = 1.0f;
-  v.varyings[2] = 1.0f;
-  v.varyings[3] = 0.5f;
-  v.varyings[4] = 1.0f;
-  v.varyings[5] = 0.1f;
+  v.varyings[2] = 0.3f;
+  v.varyings[3] = 0.3f;
+  v.varyings[4] = 0.3f;
+  v.varyings[5] = 0.3f;
   v3d_vertarr_put(&va1, 2, &v, 1);
   v.x = 400.0f; v.y = 100.0f;
   v.varyings[0] = 1.0f;
   v.varyings[1] = 0.0f;
-  v.varyings[2] = 1.0f;
-  v.varyings[3] = 1.0f;
-  v.varyings[4] = 0.5f;
-  v.varyings[5] = 1.0f;
+  v.varyings[2] = 0.3f;
+  v.varyings[3] = 0.3f;
+  v.varyings[4] = 0.3f;
+  v.varyings[5] = 0.3f;
   v3d_vertarr_put(&va1, 3, &v, 1);
 
   ua1 = v3d_unifarr_create(3);
@@ -59,15 +59,19 @@ void doda()
   extern uint8_t _binary_utils_nanikore_bin_start;
   nanikore = v3d_tex_create(512, 256, &_binary_utils_nanikore_bin_start);
 
-  va2 = v3d_vertarr_create(6, 2);
+  va2 = v3d_vertarr_create(6, 6);
+  v.varyings[2] = 1.0;
+  v.varyings[3] = 1.0;
+  v.varyings[4] = 1.0;
+  v.varyings[5] = 1.0;
   for (uint8_t i = 0; i <= 1; i++) {
-    v.x = 420.0f + i * 300;
-    v.y = 100.0f + i * 300;
-    v.varyings[0] = i * 2; v.varyings[1] = i;
+    v.x = 40.0f + i * 720;
+    v.y = 40.0f + i * 400;
+    v.varyings[0] = i - 0.3f; v.varyings[1] = i;
     v3d_vertarr_put(&va2, i * 3 + 0, &v, 1);
-    v.x = 720.0f; v.y = 100.0f; v.varyings[0] = 2.0f; v.varyings[1] = 0.0f;
+    v.x = 760.0f; v.y = 40.0f; v.varyings[0] = 0.7f; v.varyings[1] = 0.0f;
     v3d_vertarr_put(&va2, i * 3 + 1, &v, 1);
-    v.x = 420.0f; v.y = 400.0f; v.varyings[0] = 0.0f; v.varyings[1] = 1.0f;
+    v.x = 40.0f; v.y = 440.0f; v.varyings[0] = -0.3f; v.varyings[1] = 1.0f;
     v3d_vertarr_put(&va2, i * 3 + 2, &v, 1);
   }
 
@@ -107,19 +111,7 @@ void dodo(uint32_t fb)
 {
   // Render to texture
   v3d_ctx_wait(&ctx);
-  v3d_ctx_anew(&ctx, target, 0xffafcfef);
-
-  v3d_ctx_use_batch(&ctx, &batch3);
-  v3d_ctx_add_call(&ctx, &(v3d_call){
-    .is_indexed = false,
-    .num_verts = 3,
-    .start_index = 0,
-  });
-
-  v3d_ctx_issue(&ctx);
-  // Render to screen
-  v3d_ctx_wait(&ctx);
-  v3d_ctx_anew(&ctx, v3d_tex_screen(fb), 0xffbfdfff);
+  v3d_ctx_anew(&ctx, target, 0x0);
 
   v3d_ctx_use_batch(&ctx, &batch1);
 
@@ -128,7 +120,9 @@ void dodo(uint32_t fb)
     .num_verts = 3,
     .start_index = 0,
   };
-  v3d_ctx_add_call(&ctx, &call);
+  extern bool has_key;
+  for (int i = 0; i < (has_key ? 10 : 1); i++)
+    v3d_ctx_add_call(&ctx, &call);
 
   uint16_t i[3] = {1, 2, 3};
   v3d_mem_copy(&idxs, 0, i, sizeof i);
@@ -146,14 +140,19 @@ void dodo(uint32_t fb)
   v.y = 400.0f + 20.0f * sinf(angle);
   v.varyings[0] = 1.0f;
   v.varyings[1] = 1.0f;
-  v.varyings[2] = 1.0f;
-  v.varyings[3] = 0.5f;
-  v.varyings[4] = 1.0f;
-  v.varyings[5] = 0.1f;
+  v.varyings[2] = 0.3f;
+  v.varyings[3] = 0.3f;
+  v.varyings[4] = 0.3f;
+  v.varyings[5] = 0.3f;
   v3d_vertarr_put(&va1, 2, &v, 1);
 
   v3d_unifarr_puttex(&ua1, 0, nanikore, 0);
   v3d_unifarr_putf32(&ua1, 2, sinf(angle) * 0.5f + 0.5f);
+
+  v3d_ctx_issue(&ctx);
+  // Render to screen
+  v3d_ctx_wait(&ctx);
+  v3d_ctx_anew(&ctx, v3d_tex_screen(fb), 0xff000000);
 
   v3d_ctx_use_batch(&ctx, &batch2);
   v3d_ctx_add_call(&ctx, &(v3d_call){
