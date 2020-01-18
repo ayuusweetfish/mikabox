2647a5cb38ff415a2d1bb466f8fec9f009cb4b1a
Shiqing <shiqing-thu18@yandex.com>
2020-01-19 01:44:32 +0800

    Pitfall, revisit later

--- sys/swi.h
+++ sys/swi.h
@@ -4,69 +4,69 @@
 // https://gcc.gnu.org/onlinedocs/gcc/Modifiers.html
 // https://gcc.gnu.org/onlinedocs/gcc/Machine-Constraints.html
 
-#define syscall_attr __attribute__((noinline)) static uint32_t
+#define syscall_attr static inline uint32_t
 
 syscall_attr syscall0(uint32_t num)
 {
-  register uint32_t r0 __asm__ ("r0");
+  uint32_t ret;
   __asm__ __volatile__ (
     "mov r7, %[num]\n"
     "swi #0\n"
-    : "=&r,&r" (r0)     // Should always be register r0
+    : "=r,r" (ret)     // Should always be register r0
     : [num] "r,I" (num) // General register, or (8 bit rot even) immediate
-    : "r7", "lr", "memory", "r1", "r2", "r3"
+    : "r7", "lr", "memory", "r0", "r1", "r2", "r3"
   );
-  return r0;
+  return ret;
 }
 
 syscall_attr syscall1(uint32_t num, uint32_t arg0)
 {
-  register uint32_t r0 __asm__ ("r0");
+  uint32_t ret;
   __asm__ __volatile__ (
     "mov r7, %[num]\n"
     "mov r0, %[arg0]\n"
     "swi #0\n"
-    : "=&r,&r" (r0)
+    : "=r,r" (ret)
     : [num] "r,I" (num), [arg0] "r,I" (arg0)
-    : "r7", "lr", "memory", "r1", "r2", "r3"
+    : "r7", "lr", "memory", "r0", "r1", "r2", "r3"
   );
-  return r0;
+  return ret;
 }
 
 syscall_attr syscall2(uint32_t num, uint32_t arg0, uint32_t arg1)
 {
-  register uint32_t r0 __asm__ ("r0");
+  uint32_t ret;
   __asm__ __volatile__ (
     "mov r7, %[num]\n"
     "mov r0, %[arg0]\n"
     "mov r1, %[arg1]\n"
     "swi #0\n"
-    : "=&r,&r" (r0)
+    : "=r,r" (ret)
     : [num] "r,I" (num), [arg0] "r,I" (arg0), [arg1] "r,I" (arg1)
-    : "r7", "lr", "memory", "r1", "r2", "r3"
+    : "r7", "lr", "memory", "r0", "r1", "r2", "r3"
   );
-  return r0;
+  return ret;
 }
 
 syscall_attr syscall3(uint32_t num, uint32_t arg0, uint32_t arg1, uint32_t arg2)
 {
-  register uint32_t r0 __asm__ ("r0");
+  uint32_t ret;
   __asm__ __volatile__ (
     "mov r7, %[num]\n"
     "mov r0, %[arg0]\n"
     "mov r1, %[arg1]\n"
     "mov r2, %[arg2]\n"
     "swi #0\n"
-    : "=&r,&r" (r0)
+    : "=r,r" (ret)
     : [num] "r,I" (num), [arg0] "r,I" (arg0), [arg1] "r,I" (arg1), [arg2] "r,I" (arg2)
-    : "r7", "lr", "memory", "r1", "r2", "r3"
+    : "r7", "lr", "memory", "r0", "r1", "r2", "r3"
   );
-  return r0;
+  return ret;
 }
 
 syscall_attr syscall4(uint32_t num, uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3)
 {
-  register uint32_t r0 __asm__ ("r0");
+  uint32_t ret;
   __asm__ __volatile__ (
     "mov r7, %[num]\n"
     "mov r0, %[arg0]\n"
@@ -74,11 +74,11 @@ syscall_attr syscall4(uint32_t num, uint32_t arg0, uint32_t arg1, uint32_t arg2,
     "mov r2, %[arg2]\n"
     "mov r3, %[arg3]\n"
     "swi #0\n"
-    : "=&r,&r" (r0)
+    : "=r,r" (ret)
     : [num] "r,I" (num), [arg0] "r,I" (arg0), [arg1] "r,I" (arg1), [arg2] "r,I" (arg2), [arg3] "r,I" (arg3)
-    : "r7", "lr", "memory", "r1", "r2", "r3"
+    : "r7", "lr", "memory", "r0", "r1", "r2", "r3"
   );
-  return r0;
+  return ret;
 }
 
 uint32_t swi_handler(uint32_t r0, uint32_t r1, uint32_t r2, uint32_t r3);
