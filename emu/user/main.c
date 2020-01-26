fd79ae68d232d4ccbe1d10fe464fa376f92c7205
Shiqing <shiqing-thu18@yandex.com>
2020-01-26 17:18:46 +0800

    emu: Create header for user-space syscalls

--- emu/user/main.c
+++ emu/user/main.c
@@ -1,4 +1,5 @@
 #include "mikabox.h"
+#include "syscalls.h"
 
 extern unsigned char _bss_begin;
 extern unsigned char _bss_end;
@@ -16,47 +17,47 @@ __attribute__ ((noinline)) void crt_init()
 void draw()
 {
   // Create context
-  int ctx = syscall(256 + 0);
+  int ctx = gfx_ctx_create();
 
   // Create vertex arrays, uniform arrays and shader
   int va1, ua1, sh1;
-  va1 = syscall(256 + 32, 6, 3);
-  ua1 = syscall(256 + 48, 0);
-  sh1 = syscall(256 + 64, "#C");
+  va1 = gfx_varr_create(6, 3);
+  ua1 = gfx_uarr_create(0);
+  sh1 = gfx_shad_create("#C");
   int va2, ua2, sh2;
-  va2 = syscall(256 + 32, 6, 4);
-  ua2 = syscall(256 + 48, 0);
-  sh2 = syscall(256 + 64, "#CA");
+  va2 = gfx_varr_create(6, 4);
+  ua2 = gfx_uarr_create(0);
+  sh2 = gfx_shad_create("#CA");
   int va3, ua3, sh3, sh3t, tex3;
-  va3 = syscall(256 + 32, 6, 6);
-  ua3 = syscall(256 + 48, 2);
-  sh3 = syscall(256 + 64, "#TCA");
-  sh3t = syscall(256 + 64, "#T");
+  va3 = gfx_varr_create(6, 6);
+  ua3 = gfx_uarr_create(2);
+  sh3 = gfx_shad_create("#TCA");
+  sh3t = gfx_shad_create("#T");
 #define tw 96
 #define th 64
   tex3 = syscall(256 + 16, tw, th);
 
-  int ia = syscall(256 + 96, 3);
+  int ia = gfx_iarr_create(3);
 
   // Create batch
   int bat1, bat2, bat3, bat3t;
-  bat1 = syscall(256 + 80, va1, ua1, sh1);
-  bat2 = syscall(256 + 80, va2, ua2, sh2);
+  bat1 = gfx_bat_create(va1, ua1, sh1);
+  bat2 = gfx_bat_create(va2, ua2, sh2);
 
   // Close and reopen
   for (int i = 0; i < 20; i++) {
-    syscall(256 + 47, va2);
-    syscall(256 + 63, ua2);
-    syscall(256 + 79, sh2);
-    syscall(256 + 111, ia);
-    syscall(256 + 95, bat1);
-    syscall(256 + 95, bat2);
-    va2 = syscall(256 + 32, 6, 4);
-    ua2 = syscall(256 + 48, 0);
-    sh2 = syscall(256 + 64, "#CA");
-    ia = syscall(256 + 96, 3);
-    bat1 = syscall(256 + 80, va1, ua1, sh1);
-    bat2 = syscall(256 + 80, va2, ua2, sh2);
+    gfx_varr_close(va2);
+    gfx_uarr_close(ua2);
+    gfx_shad_close(sh2);
+    gfx_iarr_close(ia);
+    gfx_bat_close(bat1);
+    gfx_bat_close(bat2);
+    va2 = gfx_varr_create(6, 4);
+    ua2 = gfx_uarr_create(0);
+    sh2 = gfx_shad_create("#CA");
+    ia = gfx_iarr_create(3);
+    bat1 = gfx_bat_create(va1, ua1, sh1);
+    bat2 = gfx_bat_create(va2, ua2, sh2);
   }
 
   // Set up texture and batch 3
@@ -68,14 +69,14 @@ void draw()
       z[i][j][2] = 0xff - j;
       z[i][j][3] = 0xdd;
     }
-  syscall(256 + 17, tex3, z, 0);
+  gfx_tex_update(tex3, z, 0);
   syscall(256 + 50, ua3, 0, tex3, (2 << 0) | (2 << 2));
-  bat3 = syscall(256 + 80, va3, ua3, sh3);
-  bat3t = syscall(256 + 80, va3, ua3, sh3t);
+  bat3 = gfx_bat_create(va3, ua3, sh3);
+  bat3t = gfx_bat_create(va3, ua3, sh3t);
 
   // Populate index buffer
   uint16_t idxs[3] = {0, 1, 2};
-  syscall(256 + 97, ia, 0, idxs, 3);
+  gfx_iarr_put(ia, 0, idxs, 3);
 
   float attr[8];
   attr[3] = 1.0;
@@ -83,26 +84,26 @@ void draw()
   for (int i = 0; i <= 1; i++) {
     attr[0] = attr[1] = (i == 0 ? -0.5 : +0.5);
     attr[2] = 0.9;
-    syscall(256 + 33, va1, i * 3 + 0, &attr[0], 1);
+    gfx_varr_put(va1, i * 3 + 0, &attr[0], 1);
     attr[0] = 0.5; attr[1] = -0.5;
     attr[2] = 0.7;
-    syscall(256 + 33, va1, i * 3 + 1, &attr[0], 1);
+    gfx_varr_put(va1, i * 3 + 1, &attr[0], 1);
     attr[0] = -0.5; attr[1] = 0.5;
     attr[2] = 0.5;
-    syscall(256 + 33, va1, i * 3 + 2, &attr[0], 1);
+    gfx_varr_put(va1, i * 3 + 2, &attr[0], 1);
   }
   attr[2] = 1.0;
   attr[3] = 0.5;
   for (int i = 0; i <= 1; i++) {
     attr[0] = attr[1] = (i == 0 ? -0.5 : +0.5);
     attr[4] = 0.9; attr[5] = 1.0;
-    syscall(256 + 33, va2, i * 3 + 0, &attr[0], 1);
+    gfx_varr_put(va2, i * 3 + 0, &attr[0], 1);
     attr[0] = 0.8; attr[1] = -0.2;
     attr[4] = 0.7; attr[5] = 0.3;
-    syscall(256 + 33, va2, i * 3 + 1, &attr[0], 1);
+    gfx_varr_put(va2, i * 3 + 1, &attr[0], 1);
     attr[0] = -0.2; attr[1] = 0.8;
     attr[4] = 0.5; attr[5] = 0.3;
-    syscall(256 + 33, va2, i * 3 + 2, &attr[0], 1);
+    gfx_varr_put(va2, i * 3 + 2, &attr[0], 1);
   }
 
   attr[5] = 1.0;
@@ -111,14 +112,14 @@ void draw()
     attr[0] = attr[1] = (i == 0 ? 0.6 : 0.9);
     attr[2] = attr[3] = (i == 0 ? -1 : +2);
     attr[4] = 1.0; attr[7] = 1.0;
-    syscall(256 + 33, va3, i * 3 + 0, &attr[0], 1);
+    gfx_varr_put(va3, i * 3 + 0, &attr[0], 1);
     attr[0] = 0.6; attr[1] = 0.9;
     attr[2] = -1; attr[3] = 2;
-    syscall(256 + 33, va3, i * 3 + 1, &attr[0], 1);
+    gfx_varr_put(va3, i * 3 + 1, &attr[0], 1);
     attr[0] = 0.9; attr[1] = 0.6;
     attr[2] = 2; attr[3] = -1;
     attr[4] = 0.5; attr[7] = 0.1;
-    syscall(256 + 33, va3, i * 3 + 2, &attr[0], 1);
+    gfx_varr_put(va3, i * 3 + 2, &attr[0], 1);
   }
 
   while (1) {
@@ -131,22 +132,22 @@ void draw()
       t == 0 ? 0xffffffff : 0xffffddcc);
 
     // Use batch 1 and add call
-    syscall(256 + 2, ctx, bat1);
-    syscall(256 + 3, ctx, 0, 3, t == 0 ? 0 : 3);
+    gfx_ctx_batch(ctx, bat1);
+    gfx_ctx_call(ctx, 0, 3, t == 0 ? 0 : 3);
 
     // Use batch 2 and add call
-    syscall(256 + 2, ctx, bat2);
+    gfx_ctx_batch(ctx, bat2);
     idxs[0] = (t == 0 ? 1 : 3);
-    syscall(256 + 97, ia, 1, idxs, 1);
-    syscall(256 + 3, ctx, 1, 3, ia);
+    gfx_iarr_put(ia, 1, idxs, 1);
+    gfx_ctx_call(ctx, 1, 3, ia);
 
     // Use batch 3 and add call
     uint64_t tick = syscall64(2);
-    syscall(256 + 2, ctx, tick % 1000000 < 500000 ? bat3 : bat3t);
-    syscall(256 + 3, ctx, 0, 6, 0);
+    gfx_ctx_batch(ctx, tick % 1000000 < 500000 ? bat3 : bat3t);
+    gfx_ctx_call(ctx, 0, 6, 0);
 
     // Issue and wait
-    syscall(256 + 4, ctx);
+    gfx_ctx_issue(ctx);
     syscall(1);
   }
 }
@@ -168,7 +169,7 @@ void update()
       s[j] = "0123456789abcdef"[t & 0xf];
       t >>= 4;
     }
-    syscall(7, s);
+    mika_log(s);
   }
 }
 
@@ -176,52 +177,53 @@ void main()
 {
   crt_init();
 
-  syscall(7, "Hello world!\n");
+  mika_log("Hello world!\n");
 
   uint32_t f;
   // Write
-  f = syscall(512 + 0, "haha.txt", 0x02 | 0x08);
-  syscall(512 + 3, f, "huhuhu", 6);
-  syscall(512 + 4, f, syscall(512 + 7, f) - 1);
-  syscall(512 + 5, f);
-  syscall(512 + 1, f);
+  f = fil_open("haha.txt", 0x02 | 0x08);
+  fil_write(f, "huhuhu", 6);
+  fil_seek(f, fil_tell(f) - 1);
+  fil_trunc(f);
+  fil_close(f);
 
   // Read
   char buf[32] = { 0 };
-  f = syscall(512 + 0, "haha.txt", 0x01);
-  syscall(512 + 2, f, buf, 1);
-  syscall(7, buf);
-  uint32_t sz = syscall(512 + 9, f);
-  syscall(512 + 2, f, buf, 1);
-  syscall(7, buf);
-  syscall(512 + 4, f, sz - 2);
-  syscall(512 + 2, f, buf, sizeof(buf) - 1);
-  syscall(7, buf);
+  f = fil_open("haha.txt", 0x01);
+  fil_read(f, buf, 1);
+  mika_log(buf);
+  uint32_t sz = fil_size(f);
+  fil_read(f, buf, 1);
+  mika_log(buf);
+  fil_seek(f, sz - 2);
+  fil_read(f, buf, sizeof(buf) - 1);
+  mika_log(buf);
 
   // Rename
-  syscall(512 + 34, "haha.txt", "huhu.txt");
+  fil_rename("haha.txt", "huhu.txt");
   // Make directory
-  syscall(512 + 35, "ahahaa");
-  syscall(512 + 35, "ahahab");
+  fil_mkdir("ahahaa");
+  fil_mkdir("ahahab");
   // Stat
   buf[1] = '\0';
-  buf[0] = "-FD"[syscall(512 + 32, "ahahaa")]; syscall(7, buf);
-  buf[0] = "-FD"[syscall(512 + 32, "huhu.txt")]; syscall(7, buf);
+  buf[0] = "-FD"[fil_stat("ahahaa")]; mika_log(buf);
+  buf[0] = "-FD"[fil_stat("huhu.txt")]; mika_log(buf);
   // - Different behaviour: trailing slashes should be accepted on hardware
-  buf[0] = "-FD"[syscall(512 + 32, "ahahaa/")]; syscall(7, buf);
-  buf[0] = "-FD"[syscall(512 + 32, "zzzz")]; syscall(7, buf);
+  buf[0] = "-FD"[fil_stat("ahahaa/")]; mika_log(buf);
+  buf[0] = "-FD"[fil_stat("zzzz")]; mika_log(buf);
 
   // Open directory
   // - Both should succeed
-  syscall(512 + 16, "ahahaa");
-  syscall(7, "----");
-  uint32_t d = syscall(512 + 16, "/");
+  fil_opendir("ahahaa");
+  mika_log("----");
+  uint32_t d = fil_opendir("/");
   uint32_t ty;
-  while ((ty = syscall(512 + 18, d, buf)) != 0) {
-    if (ty == 2) syscall(7, "> dir");
-    syscall(7, buf);
+  while ((ty = fil_readdir(d, buf)) != 0) {
+    if (ty == 2) mika_log("> dir");
+    mika_log(buf);
   }
-  syscall(7, "----");
+  fil_closedir(d);
+  mika_log("----");
 
   // Unlink
   syscall(512 + 33, "ahahaa");
---
+++ emu/user/syscalls.h
@@ -0,0 +1,167 @@
+#ifndef _Mikabox_syscalls_h_
+#define _Mikabox_syscalls_h_
+
+#define offs_mika   0
+#define offs_gfx  256
+#define offs_fil  512
+
+#define argc_(_, _1, _2, _3, _4, _N, ...) _N
+#define fn__(_pfx, _argc) _pfx##_argc
+#define fn_(_pfx, _argc) fn__(_pfx, _argc)
+#define fn_argc(_pfx, ...) \
+  fn_(_pfx, argc_(_, ##__VA_ARGS__, 4, 3, 2, 1, 0))
+
+#define sep__  void,
+#define sep_i  uint32_t,
+#define sep_l  uint64_t,
+#define sep_p  void *,
+#define sep_cp const void *,
+
+#define cdr_(_h, _t) _t
+#define cdr(...) cdr_(__VA_ARGS__)
+#define name(_arg) cdr(sep_##_arg)
+#define decl__(_ty, _name) _ty _name
+#define decl_(...) decl__(__VA_ARGS__)
+#define decl(_arg) decl_(sep_##_arg)
+
+#define def_sig_body_0()
+#define def_sig_body_1(_arg) decl(_arg)
+#define def_sig_body_2(_arg, ...) decl(_arg), def_sig_body_1(__VA_ARGS__)
+#define def_sig_body_3(_arg, ...) decl(_arg), def_sig_body_2(__VA_ARGS__)
+#define def_sig_body_4(_arg, ...) decl(_arg), def_sig_body_3(__VA_ARGS__)
+
+#define def_argnames_0()
+#define def_argnames_1(_arg) , name(_arg)
+#define def_argnames_2(_arg, ...) , name(_arg) def_argnames_1(__VA_ARGS__)
+#define def_argnames_3(_arg, ...) , name(_arg) def_argnames_2(__VA_ARGS__)
+#define def_argnames_4(_arg, ...) , name(_arg) def_argnames_3(__VA_ARGS__)
+
+#define def_sig_decl_(_group, _ty, _name) _ty _group##_##_name
+#define def_sig_decl(...) def_sig_decl_(__VA_ARGS__)
+#define def_ret_(_ty, _name) return (_ty) syscall64
+#define def_ret(...) def_ret_(__VA_ARGS__)
+
+#define def__(_group, _num, _name, ...) \
+  static inline def_sig_decl(_group, sep_##_name) ( \
+    fn_argc(def_sig_body_, ##__VA_ARGS__) (__VA_ARGS__) \
+  ) { \
+    def_ret(sep_##_name) (offs_##_group + _num \
+      fn_argc(def_argnames_, ##__VA_ARGS__) (__VA_ARGS__)); \
+  }
+#define def_(_group, ...) def__(_group, __VA_ARGS__)
+#define def(...) def_(_group, __VA_ARGS__)
+
+#define _group mika
+
+def(0, _ rout, i c1, i c2, i c3, i c4)
+def(1, _ yield)
+def(2, _ tick)
+def(3, i party)
+def(4, l btns, i player)
+def(5, l axes, i player)
+def(6, l rand)
+def(7, _ log, cp str)
+
+#undef _group
+#define _group gfx
+
+def( 0, i ctx_create)
+def( 1, _ ctx_reset, i ctx, i target, i clear)
+def( 2, _ ctx_batch, i ctx, i bat)
+def( 3, _ ctx_call, i ctx, i is_indexed, i num_verts, i start_or_idxbuf)
+def( 4, _ ctx_issue)
+def( 5, _ ctx_wait)
+def(15, _ ctx_close)
+
+def(16, i tex_create, i width, i height)
+def(17, _ tex_update, i tex, cp pixels, i format)
+def(18, i tex_screen)
+def(31, _ tex_close, i tex)
+
+def(32, i varr_create, i num_verts, i num_varyings)
+def(33, _ varr_put, i varr, i start, cp verts, i num)
+def(47, _ varr_close, i varr)
+
+def(48, i uarr_create, i num_uniforms)
+def(49, _ uarr_putu32, i uarr, i index, i value)
+def(50, _ uarr_puttex, i uarr, i index, i tex, i config)
+def(63, _ uarr_close, i uarr);
+
+def(64, i shad_create, cp code)
+def(79, _ shad_close, i shad)
+
+def(80, i bat_create, i varr, i uarr, i shad)
+def(95, _ bat_close, i bat)
+
+def(96, i iarr_create, i num)
+def(97, i iarr_put, i iarr, i start, cp idxs, i num)
+def(111, i iarr_close, i iarr)
+
+#undef _group
+#define _group fil
+
+def( 0, i open, cp path, i flags)
+def( 1, _ close, i f)
+def( 2, i read, i f, p buf, i len)
+def( 3, i write, i f, cp buf, i len)
+def( 4, _ seek, i f, i pos)
+def( 5, _ trunc, i f)
+def( 6, _ flush, i f)
+def( 7, i tell, i f)
+def( 8, i eof, i f)
+def( 9, i size, i f)
+def(10, i err, i f)
+def(16, i opendir, cp path)
+def(17, _ closedir, i d)
+def(18, i readdir, i d, p name)
+def(32, i stat, cp path)
+def(33, _ unlink, cp path)
+def(34, _ rename, cp path_old, cp path_new)
+def(35, _ mkdir, cp path)
+
+#undef _group
+
+#undef offs_mika
+#undef offs_gfx
+#undef offs_fil
+
+#undef argc_
+#undef fn__
+#undef fn_
+#undef fn_argc
+
+#undef sep__
+#undef sep_i
+#undef sep_l
+#undef sep_p
+#undef sep_cp
+
+#undef cdr_
+#undef cdr
+#undef name
+#undef decl__
+#undef decl_
+#undef decl
+
+#undef def_sig_body_0
+#undef def_sig_body_1
+#undef def_sig_body_2
+#undef def_sig_body_3
+#undef def_sig_body_4
+
+#undef def_argnames_0
+#undef def_argnames_1
+#undef def_argnames_2
+#undef def_argnames_3
+#undef def_argnames_4
+
+#undef def_sig_decl_
+#undef def_sig_decl
+#undef def_ret_
+#undef def_ret
+
+#undef def__
+#undef def_
+#undef def
+
+#endif
