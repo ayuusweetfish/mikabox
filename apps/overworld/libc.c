95d3c57986f060ece54db7fd4dd0c7f1d82d9b33
Shiqing <shiqing-thu18@yandex.com>
2020-02-09 16:29:20 +0800

    apps: Properly implement _sbrk()
    ... remember to clear all .bss.* sections

--- apps/overworld/libc.c
+++ apps/overworld/libc.c
@@ -26,33 +26,14 @@ int printf(const char *format, ...)
 // void strtod() { }
 // void strtoll() { }
 clock_t clock() { return mika_wall() * CLOCKS_PER_SEC / 1000000; }
-extern uint8_t _initial_brk;
-void *realloc(void *ptr, size_t size)
-{
-  static uint32_t a[1 << 20] __attribute__ ((aligned(8)));
-  static uint32_t c = 0;
-
-  uint32_t osize = (ptr == 0 ? 0 : *((uint32_t *)ptr - 1));
-  if (size < osize) return ptr;
-
-  a[c] = size;
-  void *ret = &a[c + 1];
-  c += (size + 3) / 4 + 1;
-  c += (c & 1);
-  //char s[256];
-  //snprintf(s, 256, "%010p %3u - %p %3zd / %p\n",
-  //  ptr, osize, ret, size, (char *)a + sizeof a);
-  //mika_log(0, s);
-  if (ptr != 0) memcpy(ret, ptr, osize);
-  memset(ret + osize, 0, size - osize);
-  return ret;
-}
-void *__wrap__malloc_r(void *_unused, size_t size)
-{
-  return realloc(0, size);
-}
-void __wrap__free_r(void *_unused, void *ptr)
-{
-}
 // void __aeabi_l2d() { }
 // void __aeabi_uidivmod() { }
+
+extern uint8_t _initial_brk;
+void *_sbrk(intptr_t increment)
+{
+  static intptr_t brkdiff = 0;
+  void *ret = &_initial_brk + brkdiff;
+  if ((brkdiff += increment) < 0) brkdiff = 0;
+  return ret;
+}
